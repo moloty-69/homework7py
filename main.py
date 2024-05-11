ed8b67852589a5522e73f74608d544ef236aa6d1
moloty-69 <moloty-69@yandex.ru>
2024-05-11 18:41:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
-#x=38
-#print("драути")
-#if x<0:
-#  print("Менше нуля")
-#print("досвидания")
+x = 2
+y = x * x + 1
+is_big = x >= 3000
+
+x = [-1]
+print (x)
+my_list = [2,3,4,5,6]
+
+x , y = 3 , 8
+if x ==  3:
+  print (42)
+
+if x < 0:
+    if y > 0:
+        print ("налево")
+    else:
+        print ("направо")
+else:
+     print ("стой")
 
 
-#a,b=10,5
-
-#if a>b:
-  #print("a>b")
-
-#if a>b and a>0:
-  #print("успех")
-
-#if (a>b) and (a>0 or b<1000):
-  #print("успех")
-
-#if 5<b and b<10:
-  #print("успех")
-
-
-if "34" > "123":
-    print("успех")
-
-if "123" > "12":
-    print("успех")
-
-if [1,2] >[1,1]:
-    print("успех")
-
-if "6" > "5":
-    print("успех")
-
-if [5,6] > [5]:
-    print("успех")
-
-if "6" != 5:
-    print("успех")
+  
